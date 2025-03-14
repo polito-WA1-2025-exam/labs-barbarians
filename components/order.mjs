7a06aaea620ee85db6b3a9e660e8d6b864c646d6
vikstrr1 <rasmus.vikstrom@aalto.fi>
2025-03-14 11:37:50 +0100

    order price updates

--- components/order.mjs
+++ components/order.mjs
@@ -3,14 +3,16 @@ import { Bowl } from "./bowl.mjs";
 
 function Order() {
     this.bowls = [];
+    this.price = 0;
 
     this.addBowl = function(bowl) {
         if (!(bowl instanceof Bowl)) {
             throw new Error("Invalid bowl");
         }
-
         this.bowls.push(bowl);
+        this.price += bowl.price();
     }
+
 }
 
 export { Order };
