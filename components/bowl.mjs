0b93019cabd2d126a545a0c8df1e97363edf0a6d
henrykalff <henrykalff@gmx.de>
2025-03-15 13:17:45 +0100

    Refactoring: Add docs to bowl and order.

--- components/bowl.mjs
+++ components/bowl.mjs
@@ -1,25 +1,43 @@
 import {bases, proteines, ingredients} from "./ingredients.mjs";
 
+/**
+ * On the one hand this object stores all size for a bowl. On the other hand it stores also the number of proteins and ingredients for each bowl size.
+ */
 const bowl_sizes = {
     regular: {num_proteins: 1, num_ingredients: 4},
     medium: {num_proteins: 2, num_ingredients: 4},
     big: {num_proteins: 3, num_ingredients: 6}
 }
 
+/**
+ * The Bowl object represents one bowl. Thereby it stores the size, base, proteiens and ingredients and determine its prices. The bowl object cannot be identified by an ID but by a string that is created with the provided toString method. 
+ * @param {bowl_sizes} size - Determines the size of the bowl.
+ * @param {bases} base - Provides the base for this bowl.
+ */
 function Bowl (size, base) {
+    // Checks the validity of the passed size.
     if (!(size in bowl_sizes)) {
         throw new Error("Invalid bowl size");
     }
 
+    // Checks the validity of the passed base.
     if (!base in bases) {
         throw new Error("Invalid base");
     }
 
+    // Store size and base of the bowl. 
     this.size = size;
     this.base = base;
+
+    // Creates list for storing proteines and ingredients.
     this.proteines = [];
     this.ingredients = [];
 
+    /**
+     * This method should be used to add proteines to the bowl.
+     * @param {*} proteine - proteine to add
+     * @throws {Error} - Throws an error if the provided proteine is not in `proteines` or if the maximum number of proteines is exceeded.
+     */
     this.addProteine = function(proteine) {
         if (!(proteine in proteines)) {
             throw new Error("Invalid proteine");
@@ -32,7 +50,11 @@ function Bowl (size, base) {
         this.proteines.sort();
     }
 
-    
+    /**
+     * This method should be used to add ingredients to the bowl. 
+     * @param {*} ingredient - ingredient that should be added to the bowl.
+     * @throws {Error} - Throws an error if the provided ingredient is not in `ingredients` or if the maximum number of ingredients is exceeded.
+     */
     this.addIngredient = function(ingredient) {
         if (!(ingredient in ingredients)) {
             throw new Error("Invalid ingredient");
@@ -45,7 +67,11 @@ function Bowl (size, base) {
         this.ingredients.sort();
     }
 
-    this.price = function(size) {
+    /**
+     * This method computes the price for the current bowl.
+     * @returns price of the bowl
+     */
+    this.price = function() {
 
         let price = 0;
         if (this.size === 'regular') {
@@ -59,6 +85,11 @@ function Bowl (size, base) {
         return price;
     }
 
+    /**
+     * Creates a characteristic string that specifies the bowl-object. A bowl object is specified by its properties as size, base and stored proteines and ingredients. This means that two independently created bowl objects having the same property are observed as equal.
+     * 
+     * @returns The characteristic string for this bowl.
+     */
     this.toString = function() {
         return `Bowl size: ${this.size}, base: ${this.base}, proteines: ${this.proteines.join(", ")}, ingredients: ${this.ingredients.join(", ")}`;
     }
--- components/order.mjs
+++ components/order.mjs
@@ -1,6 +1,8 @@
 import { Bowl } from "./bowl.mjs";
 
-
+/**
+ * The Order object should be used to handle a order. It stores the current bowls, determine the price and provide method for adjusting the bowls 
+ */
 function Order() {
     // One Order contains several bowls. Thereby, each bowl element is a tuple of the bowl object and its quantity. [bowl, num]
     this.bowls = [];
@@ -13,8 +15,6 @@ function Order() {
      * 
      * @param {Bowl} bowl - The bowl to be added.
      * @throws {Error} Throws an error if the provided object is not an instance of `Bowl`. 
-     * 
-     * 
      * 
      */
     this.addBowl = function(bowl) {
