9998b1aef20880703ec5b94daf0fe580c3a1059b
alice-stattin <alice.stattin@hotmail.com>
2025-03-19 15:58:39 +0100

    added bransh with undone code

--- createDB.mjs
+++ createDB.mjs
@@ -1,22 +1,21 @@
 import sqlite from 'sqlite3'
 
-const db = new sqlite.Database('poke.sqlite', (err) => { if (err) throw err});
-
+const db_init = new sqlite.Database('poke.sqlite', (err) => { if (err) throw err});
 // Function for removing the database tables
 
 const deleteDatabaseTables = () => {
-    return new Promise(resolve => {
-        db.serialize(() => {
-            db.run(`DROP TABLE IF EXISTS users`, (err) => {
+    return new Promise((resolve,reject) => {
+        db_init.serialize(() => {
+            db_init.run(`DROP TABLE IF EXISTS users`, (err) => {
                 if (err) reject(err);
             });
-            db.run(`DROP TABLE IF EXISTS orders`, (err) => {
+            db_init.run(`DROP TABLE IF EXISTS orders`, (err) => {
                 if (err) reject(err);
             });
-            db.run(`DROP TABLE IF EXISTS bowls_ordered`, (err) => {
+            db_init.run(`DROP TABLE IF EXISTS bowls_ordered`, (err) => {
                 if (err) reject(err);
             });
-            db.run(`DROP TABLE IF EXISTS bowls_stock`, (err) => {
+            db_init.run(`DROP TABLE IF EXISTS bowls_stock`, (err) => {
                 if (err) reject(err);
             });
 
@@ -28,16 +27,16 @@ const deleteDatabaseTables = () => {
 // Function for creating the database tables
 
 const createDatabaseTables = () => {
-    return new Promise(resolve => {
-        db.serialize(() => {
-            db.run(`CREATE TABLE IF NOT EXISTS users (
+    return new Promise((resolve, reject) => {
+        db_init.serialize(() => {
+            db_init.run(`CREATE TABLE IF NOT EXISTS users (
                 username TEXT PRIMARY KEY,
                 passwordHash TEXT NOT NULL
             )`, (err) => {
                 if (err) reject(err);
             });
 
-            db.run(`CREATE TABLE IF NOT EXISTS orders (
+            db_init.run(`CREATE TABLE IF NOT EXISTS orders (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 userId TEXT,
                 totPrice INTEGER NOT NULL,
@@ -48,7 +47,7 @@ const createDatabaseTables = () => {
                 if (err) reject(err);
             });
 
-            db.run(`CREATE TABLE IF NOT EXISTS bowls_ordered (
+            db_init.run(`CREATE TABLE IF NOT EXISTS bowls_ordered (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 orderId INTEGER,
                 size TEXT NOT NULL,
@@ -61,7 +60,7 @@ const createDatabaseTables = () => {
                 if (err) reject(err);
             });
 
-            db.run(`CREATE TABLE IF NOT EXISTS bowls_stock (
+            db_init.run(`CREATE TABLE IF NOT EXISTS bowls_stock (
                 size TEXT PRIMARY KEY,
                 nrBowlsLeft INTEGER NOT NULL,
                 price INTEGER NOT NULL,
@@ -78,20 +77,20 @@ const createDatabaseTables = () => {
 // Function for reinitalizing the database tables
 
 async function recreateDatabaseTables() {
-
 const res_del = await deleteDatabaseTables();
 const res_cre = await createDatabaseTables();
 
-db.run(`INSERT INTO bowls_stock(size, nrBowlsLeft, price, nrProteins, nrIngredients)
+db_init.run(`INSERT INTO bowls_stock(size, nrBowlsLeft, price, nrProteins, nrIngredients)
     VALUES 
     ("R", 10 ,9 , 1, 4),
     ("M", 8 ,11 , 2, 4),
     ("L", 6 ,14 , 3, 6);
 `);
 
+return db_init.close()
 }
 
 //Used to reset the database tables if wanted (and to create them first time running)
 
-recreateDatabaseTables()
+recreateDatabaseTables().then(console.log)
 
---
+++ handleDB.mjs
@@ -0,0 +1,59 @@
+
+
+import sqlite from 'sqlite3'
+
+
+function DBmanager() {
+        
+    const db = new sqlite.Database('poke.sqlite', (err) => { if (err) throw err});  
+
+    this.addUser = (username,passwordHash) => {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        return new Promise((resolve, reject) => {
+            const sql =
+                `INSERT INTO users(username, passwordHash)
+                VALUES (?,?)`
+                
+            db.run(sql, [this.username, this.passwordHash], function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ id: this.lastID, username: username });
+                }
+                });
+            });
+        };
+    
+    this.authUser = (username,passwordHash) => {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT * 
+                FROM users
+                WHERE username = ?`   
+
+            db.all(sql, [this.username], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Answer(item.username, item.passwordHash))
+                    if(!result.lenght){
+                        reject("You need to create an account")
+                    }else { 
+                        console.log(result)
+                        resolve(result)
+                    }
+                   
+                }
+            })
+
+        })
+    }
+}
+
+const user = new DBmanager().authUser("alice", "fbvksfvb")
+ 
+    
